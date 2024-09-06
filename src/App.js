673d330b1609343bf9436f7c698beaaddae28abd
Jose A Ortiz <joseangelortiz15@gmail.com>
2024-09-05 22:29:51 -0600

    Added Load more data component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Accordion from './components/accordion';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-button';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -14,7 +15,9 @@ function App() {
       {/* StarRating component */}
       {/* <StarRating /> */}
       {/* Image Slider component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={'10'}/>
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={'10'}/> */}
+      {/* Load more products component */}
+      <LoadMoreData />
     </div>
   );
 }
