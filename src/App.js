a3093b19a018d640ee70bb9066db8467d90c4d6a
Jose A Ortiz <joseangelortiz15@gmail.com>
2024-09-09 00:41:04 -0600

    Added tree view component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-button';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data';
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
       {/* Image Slider component */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={'10'}/> */}
       {/* Load more products component */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+      {/* Tree View component */}
+      <TreeView menus={menus}/>
     </div>
   );
 }
